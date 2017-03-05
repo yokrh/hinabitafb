f65c29965e3d838f76ff737cbd2a1a871503fa80
yokrh <y.oka.gml@gmail.com>
2017-03-05 08:58:02 +0000

    modify helper text_url_to_link

--- app/helpers/hinabita_helper.rb
+++ app/helpers/hinabita_helper.rb
@@ -2,10 +2,14 @@ require 'uri'
 
 module HinabitaHelper
   def text_url_to_link(text)
-    URI.extract(text).uniq.each do |url|
-      sub_text = ""
-      sub_text << "<a href=" << url << " target=\"_blank\">" << url << "</a>"
-      text.gsub!(url, sub_text)
+    begin
+      URI.extract(text).uniq.each do |url|
+        sub_text = ""
+        sub_text << "<a href=" << url << " target=\"_blank\">" << url << "</a>"
+        text.gsub!(url, sub_text)
+      end
+    rescue
+      return text
     end
     text.html_safe
   end
